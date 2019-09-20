83bd685ea039f99b23716bc82eeb8157ce4c2690
carlos raynier adames <raynieradames15@gmail.com>
2019-09-19 22:05:18 -0400

    Add PropertyPage and Arreglo de imagen

--- MyLeasing.Prism/MyLeasing.Prism/ViewModels/PropertiesPageViewModel.cs
+++ MyLeasing.Prism/MyLeasing.Prism/ViewModels/PropertiesPageViewModel.cs
@@ -11,16 +11,18 @@ namespace MyLeasing.Prism.ViewModels
 {
     public class PropertiesPageViewModel : ViewModelBase
     {
+        private readonly INavigationService _navigationService;
         private OwnerResponse _owner;
-        private ObservableCollection<PropertyResponse> _properties;
+        private ObservableCollection<PropertyItemViewModel> _properties;
 
         public PropertiesPageViewModel(
             INavigationService navigationService): base(navigationService)
         {
             Title = "Properties";
+            _navigationService = navigationService;
         }
 
-        public ObservableCollection<PropertyResponse> Properties
+        public ObservableCollection<PropertyItemViewModel> Properties
         {
             get => _properties;
             set => SetProperty(ref _properties, value);
@@ -33,7 +35,22 @@ namespace MyLeasing.Prism.ViewModels
             {
                 _owner = parameters.GetValue<OwnerResponse>("owner");
                 Title = $"Properties of: {_owner.FullName}";
-                Properties = new ObservableCollection<PropertyResponse>(_owner.Properties);
+                Properties = new ObservableCollection<PropertyItemViewModel>(_owner.Properties.Select(p => new PropertyItemViewModel(_navigationService)
+                {
+                    Address = p.Address,
+                    Contracts = p.Contracts,
+                    HasParkingLot= p.HasParkingLot,
+                    Id = p.Id,
+                    IsAvailable = p.IsAvailable,
+                    Neighborhood = p.Neighborhood,
+                    Price = p.Price,
+                    PropertyImages = p.PropertyImages,
+                     PropertyType = p.PropertyType,
+                     Remarks = p.Remarks,
+                     Rooms = p.Rooms,
+                     SquareMeters = p.SquareMeters,
+                     Stratum = p.Stratum
+                }).ToList());
             }
         }
     }
---
+++ MyLeasing.Prism/MyLeasing.Prism/ViewModels/PropertyPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using MyLeasing.Common.Models;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyLeasing.Prism.ViewModels
+{
+    public class PropertyPageViewModel : ViewModelBase
+    {
+        private PropertyResponse _property;
+        public PropertyPageViewModel(
+            INavigationService navigationService): base(navigationService)
+        {
+            Title = "Property";
+        }
+
+        public PropertyResponse Property { get => _property; set => SetProperty(ref _property, value); }
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+            if (parameters.ContainsKey("property"))
+            {
+                Property = parameters.GetValue<PropertyResponse>("property");
+                Title = $"Property: {Property.Neighborhood}";
+
+            }
+        }
+    }
+}
